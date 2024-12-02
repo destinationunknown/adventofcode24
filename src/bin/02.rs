ad81b0a164b3cfd9e85782e390e92010eda84404
Varun Ramakrishnan <ravarun3@gmail.com>
2024-12-02 13:55:56 -0500

    Solve Day 2 Part 2

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -24,7 +24,32 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let reports: Vec<Vec<i32>> = input
+        .lines()
+        .filter_map(|line| {
+            Some(
+                line.split_whitespace()
+                    .filter_map(|x| x.parse::<i32>().ok())
+                    .collect::<Vec<i32>>(),
+            )
+        })
+        .collect();
+
+    let is_valid = |report: &[i32]| {
+        report.windows(2).all(|w| (1..=3).contains(&(w[1] - w[0])))
+            || report.windows(2).all(|w| (1..=3).contains(&(w[0] - w[1])))
+    };
+
+    let num_safe: u32 = reports
+        .iter()
+        .filter(|report| {
+            (0..report.len()).any(|i| {
+                is_valid(&report[..i].iter().chain(&report[i+1..]).copied().collect::<Vec<i32>>())
+            })
+        })
+        .count() as u32;
+
+    Some(num_safe)
 }
 
 #[cfg(test)]
@@ -40,6 +65,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(4));
     }
 }
