2e99e81320331628bef6b1193688ddfed0632e5e
Varun Ramakrishnan <ravarun3@gmail.com>
2024-12-04 01:15:27 -0500

    Solve Day 3 Part 2

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -17,7 +17,21 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let without_newlines = input.replace('\n', "");
+    let dont_do_re = Regex::new(r"don't\(\).*?(do\(\)|$)").unwrap();
+    let mul_re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+
+    let filtered = dont_do_re.replace_all(&without_newlines, "");
+    let result: u32 = mul_re
+        .captures_iter(&filtered)
+        .filter_map(|x| {
+            let first = x[1].parse::<u32>().ok()?;
+            let second = x[2].parse::<u32>().ok()?;
+            Some(first * second)
+        })
+        .sum();
+
+    Some(result)
 }
 
 #[cfg(test)]
@@ -32,7 +46,9 @@ mod tests {
 
     #[test]
     fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 2,
+        ));
+        assert_eq!(result, Some(48));
     }
 }
