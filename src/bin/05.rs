a00455d5a4bec8de63d4148af92da0f8ac026b0c
Varun Ramakrishnan <ravarun3@gmail.com>
2024-12-22 01:32:46 -0500

    Solve Day 5 Part 2

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -42,7 +42,56 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let parts = input.splitn(2, "\n\n").collect::<Vec<&str>>();
+    let rules_section = parts[0];
+    let updates_section = parts[1];
+    let rules: Vec<(i32, i32)> = rules_section
+        .lines()
+        .map(|line| {
+            let l: Vec<&str> = line.splitn(2, '|').collect();
+            (l[0].parse().unwrap(), l[1].parse().unwrap())
+        })
+        .collect();
+
+    let updates: Vec<Vec<i32>> = updates_section
+        .lines()
+        .map(|line| {
+            line.split(',')
+                .filter_map(|x| x.parse::<i32>().ok())
+                .collect::<Vec<i32>>()
+        })
+        .collect();
+
+    updates
+        .iter()
+        .filter(|update| {
+            // combinations will keep pairs in their relative ordering from the original vector
+            // keep all updates with any invalid pair
+            update.iter().combinations(2).any(|pair| {
+                let first = pair[0];
+                let second = pair[1];
+                let invalid = rules.iter().any(|(x, y)| first == y && second == x);
+
+                invalid
+            })
+        })
+        .map(|update| {
+            let mut sorted_update = update.clone();
+            // if a pair is invalid, swap them
+            sorted_update.sort_by(|first, second| {
+                if rules.iter().any(|(x, y)| first == y && second == x) {
+                    std::cmp::Ordering::Less
+                } else {
+                    std::cmp::Ordering::Equal
+                }
+            });
+
+            sorted_update
+        })
+        .map(|update| update[update.len() / 2])
+        .sum::<i32>()
+        .try_into()
+        .ok()
 }
 
 #[cfg(test)]
@@ -58,6 +107,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(123));
     }
 }
